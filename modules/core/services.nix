9451557a4cec25a5d90e42d8df02234a3af5711d
Josh <joshkirkpatrick12@gmail.com>
2025-06-26 16:08:30 +0100

    update

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -1,11 +1,26 @@
 { pkgs, ... }:
 {
+
+  
+  environment.systemPackages = [(
+    pkgs.catppuccin-sddm.override {
+      flavor = "mocha";
+      font  = "Noto Sans";
+      fontSize = "9";
+      background = "${./wallpaper.png}";
+      loginBackground = true;
+    }
+  )];
+
   services.displayManager = {
-   enable = true;
-   sddm.enable = true;
    defaultSession = "hyprland";
   };
 
+  services.displayManager.sddm {
+    enable = true;
+    theme = "catppuccin-mocha"; 
+    package = pkgs.kdePackages.sddm;
+  };
 
 
   programs = {
