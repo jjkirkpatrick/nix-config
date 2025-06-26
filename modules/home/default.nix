089a63677f50d4591f0102e3bdd6fdf5626ee13c
Josh <joshkirkpatrick12@gmail.com>
2025-06-26 16:34:40 +0100

    updates

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -2,48 +2,16 @@
 {
   imports = [
     inputs.catppuccin.homeModules.catppuccin
-    #./audacious.nix                   # music player
-    #./bat.nix                         # better cat command
-    #./browser.nix                     # firefox based browser
-    #./btop.nix                        # resouces monitor 
-    #./cava.nix                        # audio visualizer
-    #./discord.nix                     # discord
-    #./fastfetch.nix                   # fetch tool
-    #./flow.nix                        # terminal text editor
-    #./fzf.nix                         # fuzzy finder
-    #./gaming.nix                      # packages related to gaming
-    #./ghostty.nix                     # terminal
-    #./git.nix                         # version control
-    #./gnome.nix                       # gnome apps
-    #./gtk.nix                         # gtk theme
     ./hyprland                        # window manager
-    ./kitty.nix                       # terminal
-    ./brave.nix
-    #./lazygit.nix
-    #./micro.nix                       # nano replacement
-    #./nemo.nix                        # file manager
-    #./nix-search/nix-search.nix       # TUI to search nixpkgs
-    #./nvim.nix                        # neovim editor
-    #./obsidian.nix
-    #./p10k/p10k.nix
     ./packages                        # CLI and GUI packages
-    #./retroarch.nix  
-    ./rofi.nix                        # launcher
-    ./waybar.nix                      # status bar
+    ./kitty.nix                       # terminal
     ./hyprpaper.nix                   # wallpaper manager
-    ./zsh                             # shell configuration
-    #./scripts/scripts.nix             # personal scripts
-    #./ssh.nix                         # ssh config
-    #./superfile/superfile.nix         # terminal file manager
-    #./swaylock.nix                    # lock screen
-    #./swayosd.nix                     # brightness / volume wiget
-    #./swaync/swaync.nix               # notification deamon
-    # ./viewnior.nix                    # image viewer
-    #./vscodium                        # vscode fork
-    #./waybar                          # status bar
-    #./waypaper.nix                    # GUI wallpaper picker
-    #./xdg-mimes.nix                   # xdg config
-    #./zsh                             # shell
+    ./waybar.nix                      # status bar
+    ./rofi.nix                        # launcher
+    ./brave.nix
+    ./p10k/p10k.nix
+    ./zsh
+                                 # shell configuration
   ];
 
   # Required home-manager options
